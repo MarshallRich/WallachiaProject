ed51396f1e8ea619a6e8839b1ec5a9f55baddd6d
MarshallRich <rlmarsh@g.clemson.edu>
2016-10-30 21:18:40 -0400

    Added a bit of file organization and the controller which will be expanded upon later

---
+++ src/main/java/com/fantasy/controllers/WallachiaProjectController.java
@@ -0,0 +1,11 @@
+package com.fantasy.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by MacLap on 10/30/16.
+ */
+
+@RestController
+public class WallachiaProjectController {
+}
