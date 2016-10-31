17a46c373ff21053bf81ef8daba3f45a60f122da
MarshallRich <rlmarsh@g.clemson.edu>
2016-10-30 21:08:35 -0400

    Initial commit

---
+++ src/main/java/com/fantasy/WallachiaProjectApplication.java
@@ -0,0 +1,12 @@
+package com.fantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallachiaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallachiaProjectApplication.class, args);
+	}
+}
